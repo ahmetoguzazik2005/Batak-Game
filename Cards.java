3ea42872313da3255f1c3886bd30a9ca90aaa9e0
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2024-09-28 17:51:30 +0300

    10

--- Cards.java
+++ Cards.java
@@ -32,7 +32,7 @@ public class Cards {
     public void shuffle(){
         Random random = new Random();
         for ( int i = 1; i <= 52; i++ ){
-        int randomNumber = random.nextInt(deck.size()) + 1;
+        int randomNumber = random.nextInt(gameDeck.size()) + 1;
 
 
 
