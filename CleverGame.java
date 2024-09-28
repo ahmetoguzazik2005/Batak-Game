32d7a27c3ecc7d2b3a74339de6edc27ce9b2914a
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2024-09-28 16:48:58 +0300

    3

--- CleverGame.java
+++ CleverGame.java
@@ -6,10 +6,10 @@ public class CleverGame{
     Cards cards3;
     Cards cards4;
 
-    Player player1;
-    Player player2;
-    Player player3;
-    Player player4;
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private Player player4;
 
     ScoreCards scordecard1;
     ScoreCards scordecard2;
@@ -22,6 +22,62 @@ public class CleverGame{
         this.player3 = player3;
         this.player4 = player4;
 
+    }
+    public boolean playTurn(Player player, Card card){
+
+
+    }
+    public boolean isTurnOf(Player player){
+
+    }
+    public boolean isGameOver(){
+
+    }
+    public int getScore(){
+
+    }
+    public  String getName(int playerNumber){
+
+    }
+    public int getRoundNo(){
+
+    }
+    public int getTurnOffPlayerNo(){
+
+    }
+    public Player getWinner(){
+        
+    }
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Player getPlayer3() {
+        return player3;
+    }
+
+    public void setPlayer3(Player player3) {
+        this.player3 = player3;
+    }
+
+    public Player getPlayer4() {
+        return player4;
+    }
+
+    public void setPlayer4(Player player4) {
+        this.player4 = player4;
     }
 
 
