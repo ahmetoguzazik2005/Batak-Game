c115b465329e7d9956839a4da0070244f61d4d35
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2024-09-28 17:21:07 +0300

    5

--- CleverGame.java
+++ CleverGame.java
@@ -1,20 +1,20 @@
 public class CleverGame{
-    Cards totalCards;
+    private Cards totalCards;
 
-    Cards cards1;
-    Cards cards2;
-    Cards cards3;
-    Cards cards4;
+    private Cards cards1;
+    private Cards cards2;
+    private Cards cards3;
+    private Cards cards4;
 
     private Player player1;
     private Player player2;
-    private Player player3;
+    private Player player3; 
     private Player player4;
 
-    ScoreCards scordecard1;
-    ScoreCards scordecard2;
-    ScoreCards scordecard3;
-    ScoreCards scordecard4;
+    private ScoreCards scorecard1;
+    private ScoreCards scorecard2;
+    private ScoreCards scorecard3;
+    private ScoreCards scorecard4;
 
     CleverGame(Player player1, Player player2, Player player3, Player player4){
         this.player1 = player1;
@@ -46,7 +46,7 @@ public class CleverGame{
 
     }
     public Player getWinner(){
-        
+
     }
     public Player getPlayer1() {
         return player1;
@@ -78,6 +78,77 @@ public class CleverGame{
 
     public void setPlayer4(Player player4) {
         this.player4 = player4;
+    }
+    public Cards getTotalCards() {
+        return totalCards;
+    }
+
+    public void setTotalCards(Cards totalCards) {
+        this.totalCards = totalCards;
+    }
+
+    public Cards getCards1() {
+        return cards1;
+    }
+
+    public void setCards1(Cards cards1) {
+        this.cards1 = cards1;
+    }
+
+    public Cards getCards2() {
+        return cards2;
+    }
+
+    public void setCards2(Cards cards2) {
+        this.cards2 = cards2;
+    }
+
+    public Cards getCards3() {
+        return cards3;
+    }
+
+    public void setCards3(Cards cards3) {
+        this.cards3 = cards3;
+    }
+
+    public Cards getCards4() {
+        return cards4;
+    }
+
+    public void setCards4(Cards cards4) {
+        this.cards4 = cards4;
+    }
+
+    public ScoreCards getScorecard1() {
+        return scorecard1;
+    }
+
+    public void setScorecard1(ScoreCards scorecard1) {
+        this.scorecard1 = scorecard1;
+    }
+
+    public ScoreCards getScorecard2() {
+        return scorecard2;
+    }
+
+    public void setScorecard2(ScoreCards scorecard2) {
+        this.scorecard2 = scorecard2;
+    }
+
+    public ScoreCards getScorecard3() {
+        return scorecard3;
+    }
+
+    public void setScorecard3(ScoreCards scorecard3) {
+        this.scorecard3 = scorecard3;
+    }
+
+    public ScoreCards getScorecard4() {
+        return scorecard4;
+    }
+
+    public void setScorecard4(ScoreCards scorecard4) {
+        this.scorecard4 = scorecard4;
     }
 
 
