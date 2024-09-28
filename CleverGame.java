c3c4e0f114ea572259d9c5bd5784e9deeb94b3ce
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2024-09-28 16:34:49 +0300

    1

--- CleverGame.java
+++ CleverGame.java
@@ -1,3 +1,30 @@
 public class CleverGame{
-    
+    Cards totalCards;
+
+    Cards cards1;
+    Cards cards2;
+    Cards cards3;
+    Cards cards4;
+
+    Player player1;
+    Player player2;
+    Player player3;
+    Player player4;
+
+    ScoreCards scordecard1;
+    ScoreCards scordecard2;
+    ScoreCards scordecard3;
+    ScoreCards scordecard4;
+
+    CleverGame(Player player1, Player player2, Player player3, Player player4){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player3 = player3;
+        this.player4 = player4;
+
+    }
+
+
+
+
 }
