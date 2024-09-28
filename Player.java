7d431dab64be6bed6778dd427ff14c1dc9b719a8
hidayet başer <150721961+hidayetbaser@users.noreply.github.com>
2024-09-28 17:45:04 +0300

    10

--- Player.java
+++ Player.java
@@ -2,9 +2,8 @@ public class Player {
     private String name;// players name
     private Cards myCards;// players's card
 
-    public Player(String name, Cards myCards){
+    public Player(String name){
         setName(name);
-        setCards(myCards);
     }
     public void setName(String name){
         this.name = name;
