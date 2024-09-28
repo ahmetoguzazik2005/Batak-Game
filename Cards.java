77c99f03a4af97e68937d198118243d748b28fa4
hidayet başer <150721961+hidayetbaser@users.noreply.github.com>
2024-09-28 17:25:53 +0300

    6

--- Cards.java
+++ Cards.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 public class Cards {
-    private ArrayList<Card> cardsInHand;// each players cards
+    private ArrayList<Card> deck;// each players cards
+
+
     public Cards(){
-        cardsInHand = new ArrayList<>();
+        deck = new ArrayList<>();
     }
     public void getTopCard(){
 
@@ -12,9 +14,18 @@ public class Cards {
 
     }
     public void createFullPackOfCards(){
+        String[] suits = {"Heart", "Spade", "Club", "Diamond"};
+
+        // Adding all cards for each suit (1 to 13)
+        for (String suit : suits) {
+            for (int i = 1; i <= 13; i++) {
+                Card card = new Card(i, suit);
+                deck.add(card);
+            }
+        }
 
     }
     public void shuffle(){
-        
+
     }
 }
