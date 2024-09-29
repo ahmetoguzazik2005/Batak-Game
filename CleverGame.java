33687a744489bf2f28a110e606efbec67bada584
hidayet başer <150721961+hidayetbaser@users.noreply.github.com>
2024-09-29 09:00:55 +0300

    player turn logic added

--- CleverGame.java
+++ CleverGame.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
+
 public class CleverGame{
     private Cards totalCards;
     private Player player1;
     private Player player2;
     private Player player3; 
     private Player player4;
+    private ArrayList<Player> playerTurns = new ArrayList<>();// to determine who to play
+    private ArrayList<Card> playedCards = new ArrayList<>();// played cards at the table
+    // the reason why i chose is in round one lists are created according to cards
+    // but from round 1 turns will be determinde by winner
+    private int playTurn = 1;
 
     private ScoreCards scorecard;
-
+    
 
     CleverGame(Player player1, Player player2, Player player3, Player player4){
         setPlayer1(player1);
@@ -14,31 +21,87 @@ public class CleverGame{
         setPlayer3(player3);
         setPlayer4(player4);
     }
-   /*  public boolean playTurn(Player player, Card card){
+    // public boolean playTurn(Player player, Card card){
 
 
-    }
-    public boolean isTurnOf(Player player){
+    // }
+    // public boolean isTurnOf(Player player){
 
-    }
-    public boolean isGameOver(){
+    // }
+    // public boolean isGameOver(){
 
-    }
-    public int getScore(){
+    // }
+    // public int getScore(){
 
-    }
-    public  String getName(int playerNumber){
+    // }
+    // public  String getName(int playerNumber){
 
-    }
-    public int getRoundNo(){
+    // }
+    // public int getRoundNo(){
 
-    }
-    public int getTurnOffPlayerNo(){
+    // }
+    // public int getTurnOffPlayerNo(){
 
-    }
+    // }
     public Player getWinner(){
 
-    }*/
+    }
+    /*
+     * every turn it will change the list according to winner 
+     * at the first turn it will create according to spade two
+     */
+    public void setPlayTurn(){
+        if(playTurn == 1){
+            if(isSpadeTwo(player1.getPlayerCards())){
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+            }else if(isSpadeTwo(player2.getPlayerCards())){
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+            }else if(isSpadeTwo(player3.getPlayerCards())){
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+            }else{
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+            }
+            playTurn++;//prevent to happen again
+        }else{
+            if(getWinner() == player1){
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+                
+            }else if(getWinner() == player2){
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+            }else if(getWinner() == player3){
+                playerTurns.add(player3);
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+            }else{
+                playerTurns.add(player4);
+                playerTurns.add(player1);
+                playerTurns.add(player2);
+                playerTurns.add(player3);
+            }
+
+        }
+
+    }
+
     public Player getPlayer1() {
         return player1;
     }
@@ -78,13 +141,22 @@ public class CleverGame{
         this.totalCards = totalCards;
     }
 
-
     public ScoreCards getScorecard() {
         return scorecard;
     }
-
-
-
-
-
+    /**
+     * search for spade two
+     * @param list players deck
+     * @return isSpadetwo is this hand
+     */
+    public boolean isSpadeTwo(ArrayList<Card> list){
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).getSuit() == "Spade"){
+                if(list.get(i).getFaceValue() == 2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- Player.java
+++ Player.java
@@ -1,24 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Player {
-    private String name;// players name
+    private String name;// players name  
     private ArrayList<Card> myCards;// players's card
 
     public Player(String name, ArrayList<Card> myCards){
         setName(name);
-        setPlayerCards(myCards);
     }
     public void setName(String name){
         this.name = name;
+        setPlayerCards(myCards);
     }
     public void setPlayerCards(ArrayList<Card> myCards){
         this.myCards = myCards;
     }
+    
     public String getName(){
         return name;
     }
-    
-    // public Card playCard(){
+    public ArrayList<Card> getPlayerCards(){
+        return myCards;
+    }
+    /**
+     * allow user to play its card
+     * @return the card which is played
+     */
+    public Card playCard(){
+        Scanner scanner = new Scanner(System.in);
+
         
-    // }
+    }
+    /**
+     * it will show user to which cards can be play
+     */
+    public void playableCards(){
+
+    }
+    
+    
 
 }
